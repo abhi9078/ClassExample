ec85a06ba2328e9430b46de4c14f4349a70cf021
Abhilash <abhilashmeher1234@gmail.com>
2022-03-08 20:36:32 +0530

    added mainn file

---
+++ ClassExample/ClassExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
