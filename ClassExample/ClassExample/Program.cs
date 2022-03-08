e257222de9257ecf80faefb95465674054fca549
Abhilash <abhilashmeher1234@gmail.com>
2022-03-08 20:39:40 +0530

    Added UC1 file

--- ClassExample/ClassExample/Program.cs
+++ ClassExample/ClassExample/Program.cs
@@ -4,6 +4,13 @@ namespace ClassExample
 {
     internal class Program
     {
+        public static string FirstName()
+        {
+            Console.WriteLine("Enter Your First Name: ");
+            string fname = Console.ReadLine();
+            return fname;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
